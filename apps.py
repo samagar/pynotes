70e911c80dc122b8eb867253042907b8aa891e45
Sandeep Magar <sandi.magar@gmail.com>
2021-12-22 13:01:44 -0800

    heroku1

--- apps.py
+++ apps.py
@@ -1,5 +1,11 @@
 # Run App
+import os
 from views import app
 
+port = int(os.environ.get('PORT', 5000))
+
 if __name__ == '__main__':
-    app.run(host='localhost', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+#if __name__ == '__main__':
+#    app.run(host='localhost', port=5000, debug=True)
