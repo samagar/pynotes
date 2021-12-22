f49235b4918a91220d72255c9e861d3791d346f4
Sandeep Magar <sandi.magar@gmail.com>
2021-12-22 10:52:14 -0800

    Heroku

--- static/js/form.js
+++ static/js/form.js
@@ -1,90 +1,124 @@
 $(document).ready(function(){
+
     $("#update").click(function(event){
     event.preventDefault();
 
     var note_id = $('#note_id').text();
     var title = $('#title').val();
     var detail = tinymce.get("detail").getContent();
-    var requesttype = "Update";
-
-
+    
     $.ajax({
         data: {
             'title': title,
             'detail': detail,
             'note_id': note_id,
-            'requesttype': requesttype,
-            'savedata': requesttype
         },
         type: 'POST',
-        url: '/add',
+        url: '/update',
         success: function(response) { 
-            var mydata = response['var1']
-            $("#flashmsg").text(mydata).show();
+            location.reload()
         },
         error: function(exception) {
             alert('Exception:', exception);
         }
     });
     });
+
     $('#search').on('input', function() {
         $("#flashmsg").text("updated text").show();
-    }); 
-});
-
-$(document).ready(function(){
-    $("#delete").click(function(event){
-    event.preventDefault();
-    var note_id = $('#note_id').text();
-    var title = $('#title').val();
-//    var detail = $('#detail').val();
-    var detail = tinymce.get("detail").getContent();
-    var requesttype = "Delete";
-
-    $.ajax({
-        data: {
-            'title': title,
-            'detail': detail,
-            'note_id': note_id,
-            'requesttype': requesttype,
-            'savedata': requesttype
-        },
-        type: 'POST',
-        url: '/add',
-        success: function(response) { 
-            var mydata = response['var1']
-            $("#flashmsg").text(mydata).show();
-        },
-        error: function(exception) {
-            alert('Exception:', exception);
-        }
-    });      
-});
-
-$("#save").click(function(event){
-    event.preventDefault();
-    var note_id = $('#note_id').text();
-    var title = $('#title').val();
-    var detail = tinymce.get("detail").getContent();
-    var requesttype = "Save";
-
-
-    $.ajax({
-        data: {
-            'title': title,
-            'detail': detail,
-            'requesttype': requesttype,
-            'savedata': requesttype
-        },
-        type: 'POST',
-        url: '/add',
-        success: function(response) { 
-            var mydata = response['var1']
-            $("#flashmsg").text(mydata).show();
-        },
-        error: function(exception) {
-            alert('Exception:', exception);
-        }
     });
-});
+
+    $("#delete").click(function(event){
+        event.preventDefault();
+        var note_id = $('#note_id').text();
+    
+        $.ajax({
+            data: {
+                'note_id': note_id,
+            },
+            type: 'POST',
+            url: '/delete',
+            success: function(response) { 
+                var mydata = response['var1']
+                $("#flashmsg").text(mydata).show()
+                location.reload()
+            },
+            error: function(exception) {
+                alert('Exception:', exception);
+            }
+        });      
+    });
+
+    $("#add").click(function(event){
+        event.preventDefault();
+        var title = $('#title').val();
+        var detail = tinymce.get("detail").getContent();    
+    
+        $.ajax({
+            data: {
+                'title': title,
+                'detail': detail,
+            },
+            type: 'POST',
+            url: '/add',
+            success: function(response) { 
+                location.reload()
+            },
+            error: function(exception) {
+                alert('Exception:', exception);
+            }
+        });
+    });
+
+    $(".sidebar").click(function(event){
+        var $div = $(this);
+        var note_id = $div.attr("value")
+        var callfrom = 'select'
+        $.ajax({
+            data: {
+                'note_id': note_id
+            },
+            type: 'GET',
+            url: '/select',
+            success: function(response) { 
+                var title = response['title']
+                var detail = response['detail']
+                tinymce.get("detail").setContent(detail)
+                $("#title").val(title)
+                $("#note_id").text(note_id)
+                $("#activity").show()
+                $("#update").show()
+                $("#delete").show()
+                $("#add").css('display','none')
+            },
+            error: function(exception) {
+                alert('Exception:', exception);
+            }
+        });
+    });
+
+    $("#new").click(function(event){
+        event.preventDefault();
+        var callfrom = 'new'
+        $.ajax({
+            data: {'callfrom': callfrom},
+            type: 'POST',
+            url: '/main',
+            success: function(response) { 
+                var title = response['title']
+                var detail = response['detail']
+                tinymce.get("detail").setContent("")
+                $("#title").val("")
+                $("#note_id").text("")
+                $("#activity").hide()
+                $("#update").hide()
+                $("#delete").hide()
+                $("#add").css('display','inline')
+            },
+            error: function(exception) {
+                alert('Exception:', exception);
+            }
+        });
+    });
+    
 });
